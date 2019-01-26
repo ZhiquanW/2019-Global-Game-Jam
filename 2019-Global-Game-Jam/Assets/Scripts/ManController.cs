2e9bc48d0d47f1d9a45889540f62073ed6804619
Simin Zhan <m18752016299_1@163.com>
2019-01-26 20:16:31 +0800

    20:16

--- 2019-Global-Game-Jam/Assets/Scripts/ManController.cs
+++ 2019-Global-Game-Jam/Assets/Scripts/ManController.cs
@@ -21,6 +21,11 @@ public class ManController : MonoBehaviour
     public bool isAttacked = false;
     public float bulletSpeed = 0f;
     private float timer = 0f;
+    public float shakeInterval = 0f;
+    public float shakeVelocity = 0f;
+    private float shakeTimer = 0f;
+    private bool isShaked = false;
+    private bool shakeUp = true;
     // Start is called before the first frame update
     void Start() {
         timer = 0;
@@ -30,12 +35,24 @@ public class ManController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        if(isAttacked)
+        realtimeVelocity = PlayerRigidbody.velocity;
+        shakeTimer += Time.deltaTime;
+        if (isGround) {
+            if (shakeTimer > shakeInterval * 2 / 5) {
+                realtimeVelocity.y = -shakeVelocity;
+                shakeUp = false;
+            }
+            if (shakeTimer > shakeInterval) {
+                realtimeVelocity.y = shakeVelocity;
+                shakeUp = true;
+                shakeTimer = 0f;
+            }
+        }
+        if (isAttacked)
             timer += Time.deltaTime;
         if (timer > bulletInterval) {
             isAttacked = false;
         }
-        realtimeVelocity = PlayerRigidbody.velocity;
         //tempLocalScale = PlayerTransform.localScale;
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
@@ -46,8 +63,17 @@ public class ManController : MonoBehaviour
         } else {
             realtimeVelocity.x = 0;
         }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            //realtimeVelocity.x = Mathf.Lerp(realtimeVelocity.x, horizontalCriticalSpeed * horizontalInput, Time.deltaTime * moveAcceleration);
+            realtimeVelocity.x = -horizontalCriticalSpeed;
+            //tempLocalScale.x = horizontalInput * Mathf.Abs(PlayerTransform.localScale.x);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            realtimeVelocity.x = horizontalCriticalSpeed;
+        } else {
+            realtimeVelocity.x = 0;
+        }
         //修改目标垂直速度
-        if (verticalInput > moveBeginCriticalValue) {
+        if (Input.GetKey(KeyCode.UpArrow)) {
             if (isGround) {
                 realtimeVelocity.y = jumpVelocity;
                 isGround = false;
@@ -65,6 +91,12 @@ public class ManController : MonoBehaviour
         }
     }
 
+    /*private void OnCollisionExit2D(Collision2D _collisionObject) {
+        if (_collisionObject.gameObject.tag == Tags.Ground) {
+            isGround = false;
+        }
+    }*/
+
     void Attack() {
         if (!isAttacked && Input.GetMouseButtonDown(0)) {
             isAttacked = true;
@@ -72,8 +104,6 @@ public class ManController : MonoBehaviour
             Vector3 manWorldPos = Camera.main.WorldToScreenPoint(transform.position);
             float radian = Mathf.Atan2(Input.mousePosition.y - manWorldPos.y, Input.mousePosition.x - manWorldPos.x);
             float angle = 180 / Mathf.PI * radian;
-            angle = 0f;
-            radian = 0f;
             Vector3 tempRotation = new Vector3(0, 0, angle);
             GameObject obj = Instantiate(bullet, transform.position, Quaternion.Euler(tempRotation));
             obj.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Cos(radian) * bulletSpeed, Mathf.Sin(radian) * bulletSpeed);
