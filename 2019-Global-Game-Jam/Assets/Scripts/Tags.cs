0806bbca53273cf745d5fe3c52c5388f207c647a
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-01-27 15:57:02 +0800

    Remove Useless Project

--- 2019-Global-Game-Jam/Assets/Scripts/Tags.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Tags : MonoBehaviour
-{
-    public const string Ground = "Ground";
-    public const string Man = "Man";
-}
