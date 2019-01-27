f2b3f47dbc692fae6009b728b35ebd8a38869c39
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-01-27 15:54:05 +0800

    Complete Project

--- 2019-Global-Game-Jam/Assets/Scripts/ManController.cs
+++ 2019-Global-Game-Jam/Assets/Scripts/ManController.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class ManController : MonoBehaviour
 {
+    public GameObject pistol;
     public GameObject bullet;
     private Rigidbody2D BulletRigidbody;
     private Rigidbody2D PlayerRigidbody;
     private Transform PlayerTransform;
-    public float moveAcceleration = 0f;//角色水平移动加速度
-    public float jumpVelocity = 0f;//角色垂直跳跃速度
-    public float stopAcceleration = 0f;//物体停止加速度
-    public float horizontalCriticalSpeed = 0f;//物体水平最大移动速度限制  
-    public float moveBeginCriticalValue = 0f;//移动临界值（输入）
-    public bool isGround = true;//判断物体是否处与地面
+    public float moveAcceleration = 0f;//��ɫˮƽ�ƶ����ٶ�
+    public float jumpVelocity = 0f;//��ɫ��ֱ��Ծ�ٶ�
+    public float stopAcceleration = 0f;//����ֹͣ���ٶ�
+    public float horizontalCriticalSpeed = 0f;//����ˮƽ����ƶ��ٶ�����  
+    public float moveBeginCriticalValue = 0f;//�ƶ��ٽ�ֵ�����룩
+    public float armLength = 0f;
+    public bool isGround = true;//�ж������Ƿ������
     Vector3 realtimeVelocity;
-    float horizontalInput;//获取水平输入
-    float verticalInput;//获取垂直输入
+    float horizontalInput;//��ȡˮƽ����
+    float verticalInput;//��ȡ��ֱ����
     public float bulletInterval = 0f;
     public bool isAttacked = false;
     public float bulletSpeed = 0f;
@@ -26,6 +28,7 @@ public class ManController : MonoBehaviour
     private float shakeTimer = 0f;
     private bool isShaked = false;
     private bool shakeUp = true;
+    private bool isWall = false;
     // Start is called before the first frame update
     void Start() {
         timer = 0;
@@ -72,14 +75,14 @@ public class ManController : MonoBehaviour
         } else {
             realtimeVelocity.x = 0;
         }
-        //修改目标垂直速度
-        if (Input.GetKey(KeyCode.UpArrow)) {
+        //�޸�Ŀ�괹ֱ�ٶ�
+        if (!isWall && Input.GetKey(KeyCode.UpArrow)) {
             if (isGround) {
                 realtimeVelocity.y = jumpVelocity;
                 isGround = false;
             }
         }
-        //修改物体目标方向与速度
+        //�޸�����Ŀ�귽�����ٶ�
         //PlayerTransform.localScale = tempLocalScale;
         PlayerRigidbody.velocity = realtimeVelocity;
         Attack();
@@ -91,21 +94,50 @@ public class ManController : MonoBehaviour
         }
     }
 
-    /*private void OnCollisionExit2D(Collision2D _collisionObject) {
-        if (_collisionObject.gameObject.tag == Tags.Ground) {
-            isGround = false;
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.tag == Tags.Wall) {
+            isWall = true;
         }
-    }*/
+    }
+
+    private void OnCollisionExit2D(Collision2D _collisionObject) {
+        /*if (_collisionObject.gameObject.tag == Tags.Ground) {
+            isGround = false;
+        }*/
+        if (_collisionObject.gameObject.tag == Tags.Wall) {
+            isWall = false;
+        }
+    }
 
     void Attack() {
+        Vector3 manWorldPos = Camera.main.WorldToScreenPoint(transform.position);
+        float radian;
+        float angle;
+        Vector3 tempRotation;
+        if (Input.mousePosition.x - manWorldPos.x > 0) {
+            if (pistol.transform.localScale.y < 0)
+                pistol.transform.localScale = new Vector3(pistol.transform.localScale.x,
+                    -pistol.transform.localScale.y, pistol.transform.localScale.z);
+            radian = Mathf.Atan2(Input.mousePosition.y - manWorldPos.y, Input.mousePosition.x - manWorldPos.x);
+            angle = 180 / Mathf.PI * radian;
+            tempRotation = new Vector3(0, 0, angle);
+            pistol.transform.rotation = Quaternion.Euler(tempRotation);
+            pistol.transform.position = this.transform.position + new Vector3(armLength * Mathf.Cos(radian), armLength * Mathf.Sin(radian) + 0.3f, 0);
+        } else {
+            if (pistol.transform.localScale.y > 0)
+                pistol.transform.localScale = new Vector3(pistol.transform.localScale.x,
+                    -pistol.transform.localScale.y, pistol.transform.localScale.z);
+            radian = Mathf.Atan2(Input.mousePosition.y - manWorldPos.y, Input.mousePosition.x - manWorldPos.x);
+            angle = 180 / Mathf.PI * radian;
+            tempRotation = new Vector3(0, 0, angle);
+            pistol.transform.rotation = Quaternion.Euler(tempRotation);
+            pistol.transform.position = this.transform.position + new Vector3(armLength * Mathf.Cos(radian), armLength * Mathf.Sin(radian) + 0.3f, 0);
+        }
+
         if (!isAttacked && Input.GetMouseButtonDown(0)) {
             isAttacked = true;
             timer = 0f;
-            Vector3 manWorldPos = Camera.main.WorldToScreenPoint(transform.position);
-            float radian = Mathf.Atan2(Input.mousePosition.y - manWorldPos.y, Input.mousePosition.x - manWorldPos.x);
-            float angle = 180 / Mathf.PI * radian;
-            Vector3 tempRotation = new Vector3(0, 0, angle);
-            GameObject obj = Instantiate(bullet, transform.position, Quaternion.Euler(tempRotation));
+            GameObject obj = Instantiate(bullet, pistol.transform.position, Quaternion.Euler(tempRotation));
             obj.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Cos(radian) * bulletSpeed, Mathf.Sin(radian) * bulletSpeed);
         }
     }
---
+++ tmp/Assets/Scripts/CollisionEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionEvent : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D _colliderObject) {
+        if (_colliderObject.gameObject.tag == Tags.Ground) {
+            Destroy(gameObject);
+        }
+        if (_colliderObject.gameObject.tag == Tags.Wall) {
+            Destroy(gameObject);
+        }
+        if (_colliderObject.gameObject.tag == Tags.Bird) {
+            _colliderObject.GetComponent<BirdController>().Dead();
+            Destroy(gameObject);
+        }
+    }
+}
